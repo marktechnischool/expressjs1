52ed59e7514cc18a069d76368ebbacd8c0a31757
Marek Dziewit <marek2901@gmail.com>
2024-02-28 14:42:53 +0100

    port v2

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.get('/', (req, resp) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running on port 3000")
+    console.log(`Server is running on port ${process.env.PORT || 3000}`)
 })
